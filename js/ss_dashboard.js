b86070888dca0c799a88b642e3ecc57445b71e50
Bien Justin Mandac <justinmandac@gmail.com>
2014-09-17 11:07:38 +0800

    Dynamic disposal bin.

--- js/ss_dashboard.js
+++ js/ss_dashboard.js
@@ -1,22 +1,28 @@
 $(function () {
-    var set_cookie     = 'gridSettings'; //name of cookie
-    var cookie         = $.cookie(set_cookie);
-    var main_container = '#gridContainer'; //id of container holding main elements
-    var disp_container = '#disposal'; //id of container that will serve as the disposal bin
-    var add_container  = '#gallery'; //id of container that holds items that can be added to main_container
+    "use strict";
+    var set_cookie, cookie, main_container, disp_container, add_container;
+
+    set_cookie     = 'gridSettings'; //name of cookie
+    cookie         = $.cookie(set_cookie);
+    main_container = '#gridContainer'; //id of container holding main elements
+    disp_container = '#disposal'; //id of container that will serve as the disposal bin
+    add_container  = '#gallery'; //id of container that holds items that can be added to main_container
 
     //utilities
     function getGridSettings(container) {
-        var children  = $(container).children();
-        var arr       = [];
+        var children, arr;
+        children =  $(container).children();
+        arr      = [];
 
         $.each(children, function (i, val) {
-            var data = {
-                left: $(val).css('left'),
-                top: $(val).css('top'),
-                id: $(val).attr('id')
-            };
-            arr.push(data);
+            if (val !== undefined) {
+                var data = {
+                    left: $(val).css('left'),
+                    top: $(val).css('top'),
+                    id: $(val).attr('id')
+                };
+                arr.push(data);
+            }
 
         });
 
@@ -30,7 +36,7 @@ $(function () {
 
     function initShapeshift(container) {
         $(container).shapeshift({
-            align: 'left',
+            align: "left",
             animateOnInit: true,
             minColumns: 3
         });
@@ -49,29 +55,51 @@ $(function () {
     }
     //reconstructs HTML from cookie information
     if (cookie !== undefined) {
-        var settings = JSON.parse($.cookie('gridSettings'));
-        containerBuilder(main_container, settings); //load containers first
+        console.log('Saved settings' + cookie);
+        containerBuilder(main_container, JSON.parse(cookie)); //load containers first
         //add function for loading content based on ids
     }
 
     initShapeshift(main_container);
     $(disp_container).shapeshift({
-        align: 'left',
+        align: "left",
         animateOnInit: true,
         minColumns: 3,
         enableTrash: true
     });
     $('#gallery').shapeshift({
-        align: 'left',
+        align: "left",
         animateOnInit: true,
         minColumns: 3,
-        dragClone: true,
-        deleteClone: false
+        dragClone: true
     });
     $('.container').on('ss-drop-complete', function (e, selected) {
         //create cookie
         createGridCookie(main_container, set_cookie);
+        console.log('Cookie contents' + $.cookie(set_cookie));
+        console.log(e);
 
+    });
+
+    $(main_container).on('mousedown', function (e) {
+        console.log('Dragging in progress.');
+        $(disp_container).css('visibility', 'visible');
+        $(main_container).addClass('span8');
+        $(disp_container).addClass('span4');
+        $(main_container).trigger('ss-rearrange');
+    });
+
+    $(main_container).on('ss-rearranged', function (e) {
+        $(disp_container).css('visibility', 'hidden');
+        $(main_container).removeClass('span8');
+        $(disp_container).removeClass('span4');
+    });
+
+    $(disp_container).on('ss-trashed', function (e) {
+        console.log('Dragging done.');
+        $(disp_container).css('visibility', 'hidden');
+        $(main_container).removeClass('span8');
+        $(disp_container).removeClass('span4');
     });
     $(disp_container).on('ss-trashed', function (e, selected) {
         //create cookie
